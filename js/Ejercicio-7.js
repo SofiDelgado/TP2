bd6055f452c443c4c51be8a0989af16f074c7595
Sofi Delgado <sofi5_09@hotmail.com>
2023-03-08 02:58:49 -0300

    Se agrega ejercicio 7

---
+++ js/Ejercicio-7.js
@@ -0,0 +1,9 @@
+let numeroRepeciones = 30;
+
+for(let i= numeroRepeciones; i >0 ; i--){
+    // bucle que controle la columna
+    for(let j= i; j>0; j--){
+        document.write(i);
+    }
+    document.write('</br>');
+}
