d9c15a522ed79e56948ac5514499a600208d8b73
Sofi Delgado <sofi5_09@hotmail.com>
2023-03-08 02:09:34 -0300

    Ejercicio 5

---
+++ js/Ejercicio-5.js
@@ -0,0 +1,18 @@
+const text = "Desea seguir agregando numeros?"
+let suma = 0;
+letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+
+do
+{
+    let dni = parseInt(prompt("Introduce el DNI: "));
+    if(typeof dni == "number" && dni>= 0 && dni<=99999999)
+    {
+        resto = dni%23;
+        alert("Le corresponde la letra: " + letras[resto]);
+    }
+    else
+    {
+        alert("No es un numero valido");
+    }
+    resultado = window.confirm(text);
+}while(resultado==true);
